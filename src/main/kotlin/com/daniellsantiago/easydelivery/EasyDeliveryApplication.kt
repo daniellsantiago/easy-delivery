d1e6c38edf2f097b260455fc211e633f0e842ecf
Daniel Santiago <danielsantiago111@hotmail.com>
2023-01-29 15:16:40 -0300

    feat: initial structure

---
+++ src/main/kotlin/com/daniellsantiago/easydelivery/EasyDeliveryApplication.kt
@@ -0,0 +1,11 @@
+package com.daniellsantiago.easydelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyDeliveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasyDeliveryApplication>(*args)
+}
---
+++ src/test/kotlin/com/daniellsantiago/easydelivery/EasyDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.daniellsantiago.easydelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasyDeliveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
